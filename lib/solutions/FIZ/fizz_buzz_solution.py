812d794ff4c04d8dc7de66d048d54f67e0d1d4e9
sbx_user1051 <sbx_user1051@169.254.129.193>
2019-08-28 08:25:25 +0000

    Wed Aug 28 08:25:25 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -9,18 +9,19 @@ def deluxe(num_len, number):
 def fizz_buzz(number):
     num_len = len(set(list(str(number))))
     if number%3==0 and number%5==0:
-        deluxe(num_len, number)
+        return deluxe(num_len, number)
     elif '3' in str(number) and '5' in str(number):
-        deluxe(num_len, number)
+        return deluxe(num_len, number)
     elif number%3==0 and '5' in str(number):
-        deluxe(num_len, number)
+        return deluxe(num_len, number)
     elif number%5==0 and '3' in str(number):
-        deluxe(num_len, number)
+        return deluxe(num_len, number)
     elif number%3==0 or '3' in str(number):
         return 'fizz'
     elif number%5==0 or '5' in str(number):
         return 'buzz'
     else:
         return number
+
 
 
