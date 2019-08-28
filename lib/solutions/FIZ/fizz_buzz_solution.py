b4f7ca4c76296efcebfd2a5f54b3c56e8c4a5278
sbx_user1051 <sbx_user1051@169.254.129.193>
2019-08-28 08:52:06 +0000

    Wed Aug 28 08:52:06 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,30 +1,29 @@
 # noinspection PyUnusedLocal
 def deluxe(number):
-    if (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 != 0:
+    if ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 != 0:
         return 'fizz buzz fake deluxe'
-    elif (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 == 0:
+    elif ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 == 0:
         return 'fizz buzz deluxe'
     else:
         return 'fizz buzz'
 
 def fizz_deluxe(number):
-    if (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 != 0:
+    if ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 != 0:
         return 'fizz fake deluxe'
-    if (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 == 0:
+    if ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 == 0:
         return 'fizz deluxe'
     else:
         return 'fizz'
 
 def buzz_deluxe(number):
-    if (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 != 0:
+    if ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 != 0:
         return 'buzz fake deluxe'
-    if (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 == 0:
+    if ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 == 0:
         return 'buzz deluxe'
     else:
         return 'buzz'
 
 def fizz_buzz(number):
-    num_len = len(set(list(str(number))))
     if number%3==0 and number%5==0:
         return deluxe(number)
     elif '3' in str(number) and '5' in str(number):
@@ -38,10 +37,11 @@ def fizz_buzz(number):
     elif number%5==0 or '5' in str(number):
         return buzz_deluxe( number)
     else:
-        if (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 != 0:
+        if ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 != 0:
             return 'fake deluxe'
-        elif (number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number)) and number%2 == 0:
+        elif ((number%3==0 and '3' in str(number)) or  (number%5==0 and '5' in str(number))) and number%2 == 0:
             return 'deluxe'
         else:
             return number
+
 
