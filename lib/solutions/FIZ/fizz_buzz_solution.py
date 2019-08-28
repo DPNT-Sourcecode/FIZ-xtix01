eab1713fff2da4c59dc0106f0bcf24accd3e7c84
sbx_user1051 <sbx_user1051@169.254.129.193>
2019-08-28 07:53:01 +0000

    Wed Aug 28 07:53:01 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,9 +2,10 @@
 def fizz_buzz(number):
     if number%3==0 and number%5==0:
         return 'fizz buzz'
-    elif number%3==0:
+    elif '3' in str(number) and '5' in str(number):
+        return 'fizz buzz'
+    elif number%3==0 or '3' in str(number):
         return 'fizz'
-    elif number%5==0:
+    elif number%5==0 or '5' in str(number):
         return 'buzz'
-    else:
-        return number
+
