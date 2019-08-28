222340b2153d815f6bb96f26d696804dba6ed0a6
sbx_user1051 <sbx_user1051@169.254.129.193>
2019-08-28 07:45:13 +0000

    Wed Aug 28 07:45:13 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,11 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+    if number%3==0 and number%5==0:
+        return 'fizz buzz'
+    elif number%3==0:
+        return 'fizz'
+    elif number%5==0:
+        return 'buzz'
+    else:
+        return number
+
