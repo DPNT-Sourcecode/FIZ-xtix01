d80e2d2b2903f675ee9644309d2b6a1f2ac6ed00
sbx_user1051 <sbx_user1051@169.254.129.193>
2019-08-28 08:18:20 +0000

    Wed Aug 28 08:18:20 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,21 +1,26 @@
 # noinspection PyUnusedLocal
+def deluxe(num_len, number):
+    if num_len == 1 and number > 10:
+        return 'fizz buzz deluxe'
+    else:
+        return 'fizz buzz'
+
+
 def fizz_buzz(number):
     num_len = len(set(list(str(number))))
     if number%3==0 and number%5==0:
-        if num_len==1 and number>10:
-            return 'fizz buzz deluxe'
-        else:
-            return 'fizz buzz'
+        deluxe(num_len, number)
     elif '3' in str(number) and '5' in str(number):
-        return 'fizz buzz'
+        deluxe(num_len, number)
     elif number%3==0 and '5' in str(number):
-        return 'fizz buzz'
+        deluxe(num_len, number)
     elif number%5==0 and '3' in str(number):
-        return 'fizz buzz'
+        deluxe(num_len, number)
     elif number%3==0 or '3' in str(number):
         return 'fizz'
     elif number%5==0 or '5' in str(number):
         return 'buzz'
     else:
         return number
+
 
